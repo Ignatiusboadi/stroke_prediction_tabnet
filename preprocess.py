97ece4169686c760fb07c24a503945f71452e524
Ignatius Boadi <ignatiusboadi@gmail.com>
2025-05-26 01:00:40 +0200

    read in data, filled in missing values for bmi and dropped `other` type of gender

---
+++ preprocess.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+data = pd.read_csv('data/patients_info.csv')
+
+print('data info after reading data')
+data.info()
+data = data.drop(columns=['id'])
+data['bmi'] = data['bmi'].fillna(data['bmi'].mean())
+
+print('data info after filling in missing values')
+data.info()
+
+cat_cols = ["gender", "hypertension", "heart_disease", "ever_married", "work_type", "Residence_type", "smoking_status",
+            "stroke"]
+cont_cols = ["age", "avg_glucose_level", "bmi"]
+
+for col in cat_cols:
+    print(data[col].unique())
+
+data = data.drop(index=data.query('`gender` == "Other"').index)
+print('unique values of gender', data.gender.unique())
+
+
