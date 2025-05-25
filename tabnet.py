36701868749e08b09adc6c34d3e665bb233b9d88
Ignatius Boadi <ignatiusboadi@gmail.com>
2025-05-26 01:45:07 +0200

    created indices and dimensions lists for categorical columns

--- tabnet.py
+++ tabnet.py
@@ -7,5 +7,7 @@ import pandas as pd
 patients_data = pd.read_csv('data/smote_patients_info.csv')
 patients_data['Set'] = np.random.choice(['train', 'valid', 'test'], p=[0.8, 0.1, 0.1], size=[patients_data.shape[0]])
 
-
-print(patients_data.columns)
+cat_cols = ["gender", "hypertension", "heart_disease", "ever_married", "work_type", "Residence_type", "smoking_status",
+            "stroke"]
+cat_ind = [i for i, f in enumerate(patients_data.columns) if f in cat_cols]
+cat_dim = [patients_data[f].nunique() for f in patients_data.columns if f in cat_cols]
